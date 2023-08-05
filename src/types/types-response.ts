0b9b7ef26bc239ae540a19e03c54366a516cf7fe
Jailson Prado <jailsondpd@gmail.com>
2023-08-05 08:23:59 -0300

    feat: create use-request

---
+++ src/types/types-response.ts
@@ -0,0 +1,48 @@
+export interface Payload {
+  data: {
+    results: Character[];
+    offset: string;
+    total: string;
+  };
+  attributionText: string;
+}
+
+export interface AppContextProps {
+  onSearchHero(textSearch: string): void;
+  onPaginate(page: number): void;
+  attributionText: string;
+  characters: Character[];
+  pageInfo: { page: string; total: string };
+  loading: boolean;
+}
+
+export interface Character {
+  id: string;
+  name: string;
+  description: string;
+  thumbnail: Thumbnail;
+  comics: Details;
+  series: Details;
+  stories: Details;
+  events: Details;
+  urls: Url[];
+}
+
+interface Thumbnail {
+  path: string;
+  extension: string;
+}
+
+interface Details {
+  items: Item[];
+}
+
+interface Item {
+  name: string;
+  type: string;
+}
+
+interface Url {
+  type: string;
+  url: string;
+}
