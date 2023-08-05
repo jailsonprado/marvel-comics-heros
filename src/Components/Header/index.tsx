19c3c3275c87673eea6c90cf8addf6b8ce3faf75
Jailson Prado <jailsondpd@gmail.com>
2023-08-05 08:26:14 -0300

    feat: create structure for header page

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header: React.FC = () => (
+  <header>
+    <div>Marvel</div>
+    <p>Explore o universo</p>
+    <p>
+      Mergulhe no dominio deslumbrante de todos os personagens classicos que
+      voce ama - e aqueles que voce descobrira em breve!
+    </p>
+  </header>
+);
+
+export default Header;
