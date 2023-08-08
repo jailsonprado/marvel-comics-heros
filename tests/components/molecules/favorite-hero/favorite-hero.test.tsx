a13b2f0e68f13f5cb95ce3c9d544cf0722028d85
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:33:20 -0300

    test: create test for component favorite hero

---
+++ tests/components/molecules/favorite-hero/favorite-hero.test.tsx
@@ -0,0 +1,59 @@
+// favorite-hero.test.tsx
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { FavoriteHero } from '@/components/molecules';
+import { listOnlyFavorites } from '@/redux/heros/heros-slice';
+import { waitFor } from '@testing-library/dom';
+
+const mockStore = configureStore([]);
+
+describe('FavoriteHero', () => {
+  it('should render the component with toggle off by default', () => {
+    const store = mockStore({});
+
+    render(
+      <Provider store={store}>
+        <FavoriteHero />
+      </Provider>,
+    );
+
+    const favorito02Icon = screen.getByAltText('favorito02-icon');
+    waitFor(() => {
+      expect(favorito02Icon).toBeInTheDocument();
+    });
+
+    const favorito01Icon = screen.queryByAltText('favorito01-icon');
+    waitFor(() => {
+      expect(favorito01Icon).not.toBeInTheDocument();
+    });
+  });
+
+  it('should toggle the state and dispatch the action when the toggle button is clicked', () => {
+    const store = mockStore({});
+
+    render(
+      <Provider store={store}>
+        <FavoriteHero />
+      </Provider>,
+    );
+
+    const toggleButton = screen.getByAltText('favorito02-icon');
+
+    fireEvent.click(toggleButton);
+
+    const favorito02Icon = screen.queryByAltText('favorito02-icon');
+    waitFor(() => {
+      expect(favorito02Icon).not.toBeInTheDocument();
+    });
+
+    const favorito01Icon = screen.getByAltText('favorito01-icon');
+    waitFor(() => {
+      expect(favorito01Icon).toBeInTheDocument();
+    });
+
+    waitFor(() => {
+      expect(store.getActions()).toEqual([listOnlyFavorites(true)]);
+    });
+  });
+});
