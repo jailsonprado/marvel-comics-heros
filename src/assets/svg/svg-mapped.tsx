134c4ec7c01e74ec157e4e1932df59e6661cda8b
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:23:50 -0300

    feat: create mapped for assets svg and png

---
+++ src/assets/svg/svg-mapped.tsx
@@ -0,0 +1,40 @@
+/* istanbul ignore file */
+import AvaliacaoOffSvg from '@/assets/avaliacao_off.svg';
+import AvaliacaoOnSvg from '@/assets/avaliacao_on.svg';
+import Favorito01Svg from '@/assets/favorito_01.svg';
+import Favorito02Svg from '@/assets/favorito_02.svg';
+import Favorito03Svg from '@/assets/favorito_03.svg';
+import IcBuscaSvg from '@/assets/ic_busca.svg';
+import IcBuscaMenorSvg from '@/assets/ic_busca_menor.svg';
+import IcHeroiSvg from '@/assets/ic_heroi.svg';
+import IcQuadrinhosSvg from '@/assets/ic_quadrinhos.svg';
+import IcTrailerSvg from '@/assets/ic_trailer.svg';
+import LogoMenorSvg from '@/assets/logo_menor.svg';
+import LogoSvg from '@/assets/logo.svg';
+import SearchBarBrancoSvg from '@/assets/search_bar_branco.svg';
+import SearchBarVermelhoSvg from '@/assets/search_bar_vermelho.svg';
+import SearchPng from '@/assets/search.png';
+import ToggleOffSvg from '@/assets/toggle_off.svg';
+import ToggleOnSvg from '@/assets/toggle_on.svg';
+import DeadPool from '@/assets/dead-pool.png';
+
+export {
+  AvaliacaoOffSvg,
+  AvaliacaoOnSvg,
+  Favorito01Svg,
+  Favorito02Svg,
+  Favorito03Svg,
+  IcBuscaSvg,
+  IcBuscaMenorSvg,
+  IcHeroiSvg,
+  IcQuadrinhosSvg,
+  IcTrailerSvg,
+  LogoMenorSvg,
+  LogoSvg,
+  SearchBarBrancoSvg,
+  SearchBarVermelhoSvg,
+  SearchPng,
+  ToggleOffSvg,
+  ToggleOnSvg,
+  DeadPool,
+};
