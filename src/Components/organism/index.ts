7fc265ffafc6f0e3421481b9599a5e32856783bc
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:42:40 -0300

    feat: create export global for organism

---
+++ src/Components/organism/index.ts
@@ -0,0 +1,4 @@
+export * from '@/components/organism/footer/footer';
+export * from '@/components/organism/header/header';
+export * from '@/components/organism/navigation/navigation';
+export * from '@/components/organism/card-hero/card-hero';
