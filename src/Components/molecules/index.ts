1746d5f2ff509d83b3d46b6d8851c1520ccadb4f
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:41:03 -0300

    feat: create export global for molecules

---
+++ src/Components/molecules/index.ts
@@ -0,0 +1,5 @@
+export * from '@/components/molecules/card-hero/card-hero';
+export * from '@/components/molecules/search-home/search-home';
+export * from '@/components/molecules/favorite-hero/favorite-hero';
+export * from '@/components/molecules/order-by-name/order-by-name';
+export * from '@/components/molecules/total-hero/total-hero';
