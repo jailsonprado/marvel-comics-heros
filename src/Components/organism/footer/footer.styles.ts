19738e7969f629a02f6bffc23aa640e74ab42987
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:41:19 -0300

    feat: create footer

---
+++ src/Components/organism/footer/footer.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  background-color: #e92429;
+  padding: 20px;
+  margin-top: 30px;
+`;
---
+++ src/Components/organism/footer/footer.tsx
@@ -0,0 +1,20 @@
+import { Text } from '@/components/atoms';
+import * as S from './footer.styles';
+
+export const Footer: React.FC = () => (
+  <S.Container>
+    <Text
+      component="span"
+      color="#000"
+      text="Desenvolvido por Jailson Prado"
+      fontSize={16}
+    />
+    <Text
+      component="a"
+      color="#fff"
+      text="Data provided by Marvel. © 2023 MARVEL "
+      fontSize={14}
+      marginTop={10}
+    />
+  </S.Container>
+);
