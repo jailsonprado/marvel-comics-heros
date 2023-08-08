0d73428f7c15b6d15e5528acb6b28f84373ab9d6
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:33:02 -0300

    feat: create component favorite hero

---
+++ src/Components/molecules/favorite-hero/favorite-hero.tsx
@@ -0,0 +1,36 @@
+// favorite-hero.tsx
+import { useCallback, useEffect, useState } from 'react';
+import * as S from './favorite-hero.styles';
+import { Text, Image } from '@/components/atoms';
+import { listOnlyFavorites } from '@/redux/heros/heros-slice';
+import { Favorito01Svg, Favorito02Svg } from '@/assets/svg/svg-mapped';
+import { useAppDispatch } from '@/redux/types/types';
+
+export const FavoriteHero: React.FC = () => {
+  const [favorite, setFavorite] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+
+  const handleActiveListFavorite = useCallback(async () => {
+    return dispatch(listOnlyFavorites(favorite));
+  }, [dispatch, favorite]);
+
+  const handleOnlyFavoritesHero = () => {
+    setFavorite((prevState) => !prevState);
+    handleActiveListFavorite();
+  };
+
+  useEffect(() => {
+    handleActiveListFavorite();
+  }, [favorite, handleActiveListFavorite]);
+
+  return (
+    <S.WrapperItem>
+      <Image
+        src={!favorite ? Favorito02Svg : Favorito01Svg}
+        descriptionAlt={!favorite ? 'favorito02-icon' : 'favorito01-icon'}
+        handleClick={handleOnlyFavoritesHero}
+      />
+      <Text component="span" text="Somente favoritos" padding={10} />
+    </S.WrapperItem>
+  );
+};
