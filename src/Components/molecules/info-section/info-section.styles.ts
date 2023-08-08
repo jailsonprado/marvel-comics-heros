178fdf08917b3e48f5ab11c0025403af6b896ce6
Jailson Prado <jailsondpd@gmail.com>
2023-08-08 07:30:28 -0300

    feat: create inf-section

---
+++ src/Components/molecules/info-section/info-section.styles.ts
@@ -0,0 +1,80 @@
+import { Image, Text } from '@/components/atoms';
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 60%;
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    flex-direction: row;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 40px;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    margin-right: 0;
+    margin-bottom: 20px;
+  }
+`;
+
+export const IconInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+  min-height: 50px;
+`;
+
+export const InfoText = styled(Text)`
+  font-size: 18px;
+  font-weight: 300;
+  margin-left: ${(props) => props.marginLeft || 0}px;
+`;
+
+export const ResponsiveImage = styled(Image)`
+  @media (max-width: 768px) {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+export const StarIcon = styled(Image)`
+  width: 16px;
+  height: 16px;
+  margin-right: 8px;
+`;
+
+export const RatingSection = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const WrapperText = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
---
+++ src/Components/molecules/info-section/info-section.tsx
@@ -0,0 +1,94 @@
+import { Text } from '@/components/atoms';
+import {
+  AvaliacaoOnSvg,
+  IcQuadrinhosSvg,
+  IcTrailerSvg,
+} from '@/assets/svg/svg-mapped';
+import * as S from './info-section.styles';
+
+interface Props {
+  comicsCount?: number;
+  moviesCount?: number;
+  rating: number;
+  date?: string;
+}
+
+export const InfoSection: React.FC<Props> = ({
+  comicsCount,
+  moviesCount,
+  rating,
+  date,
+}) => {
+  const maxStars = 5;
+  const starsArray = new Array(Math.min(rating, maxStars)).fill(null);
+
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.InfoWrapper>
+          <Text
+            component="span"
+            text="Quadrinhos"
+            fontSize={18}
+            fontWeight={300}
+          />
+          <S.IconInfoWrapper>
+            <S.ResponsiveImage
+              src={IcQuadrinhosSvg}
+              descriptionAlt="comics-icon"
+              width={40}
+            />
+            <S.InfoText
+              component="span"
+              text={comicsCount?.toString()}
+              marginLeft={20}
+            />
+          </S.IconInfoWrapper>
+        </S.InfoWrapper>
+        <S.InfoWrapper>
+          <Text
+            component="span"
+            fontSize={18}
+            text="Filmes"
+            fontWeight={300}
+            width={90}
+          />
+          <S.IconInfoWrapper>
+            <S.ResponsiveImage
+              src={IcTrailerSvg}
+              descriptionAlt="movies-icon"
+              width={40}
+            />
+            <S.InfoText
+              component="span"
+              text={moviesCount?.toString()}
+              marginLeft={30}
+            />
+          </S.IconInfoWrapper>
+        </S.InfoWrapper>
+        <S.InfoWrapper>
+          <S.InfoText
+            component="span"
+            fontSize={18}
+            text="Rating"
+            fontWeight={300}
+            marginRight={20}
+          />
+          <S.IconInfoWrapper>
+            {starsArray.map((_, index) => (
+              <S.StarIcon
+                key={index}
+                src={AvaliacaoOnSvg}
+                descriptionAlt={`star-icon-${index}`}
+              />
+            ))}
+          </S.IconInfoWrapper>
+        </S.InfoWrapper>
+      </S.Container>
+      <S.WrapperText>
+        <Text component="span" text="Último quadrinho:" />
+        <Text component="span" marginLeft={20} text={date} color="#8C8C8C" />
+      </S.WrapperText>
+    </S.Wrapper>
+  );
+};
