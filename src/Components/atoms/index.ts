e5a77cabe255e3e1eb9de523c7b3c3a209f50452
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:31:53 -0300

    feat: create export all components

---
+++ src/Components/atoms/index.ts
@@ -0,0 +1,4 @@
+export * from '@/components/atoms/image/image';
+export * from '@/components/atoms/loading/loading';
+export * from '@/components/atoms/text/text';
+export * from '@/components/atoms/pagination/pagination';
