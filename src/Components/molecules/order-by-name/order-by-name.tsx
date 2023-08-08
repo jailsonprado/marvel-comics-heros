940703ad5817041b8ec1c3320bf4709d81bb26ea
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:33:42 -0300

    feat: create component orderBy

---
+++ src/Components/molecules/order-by-name/order-by-name.tsx
@@ -0,0 +1,29 @@
+// order-by-name.tsx
+import { Text, Image } from '@/components/atoms';
+import * as S from './order-by-name.styles';
+import { IcHeroiSvg, ToggleOffSvg, ToggleOnSvg } from '@/assets/svg/svg-mapped';
+import { useState } from 'react';
+import { useAppDispatch } from '@/redux/types/types';
+import { toggleActive } from '@/redux/heros/heros-slice';
+
+export const OrderByName: React.FC = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+
+  const onToggle = () => {
+    setToggle((prevState) => !prevState);
+    dispatch(toggleActive(toggle));
+  };
+
+  return (
+    <S.WrapperItem>
+      <Image src={IcHeroiSvg} descriptionAlt="hero-icon" />
+      <Text component="span" text="Ordenar por nome - A/Z" padding={10} />
+      <Image
+        src={!toggle ? ToggleOffSvg : ToggleOnSvg}
+        descriptionAlt={!toggle ? 'toggle-off-icon' : 'toggle-on-icon'}
+        handleClick={onToggle}
+      />
+    </S.WrapperItem>
+  );
+};
