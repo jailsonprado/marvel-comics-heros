09363bdbbac6983a0d385c69f405b591d744044b
Jailson Prado <jailsondpd@gmail.com>
2023-08-08 09:35:18 -0300

    feat: create page for details hero

---
+++ src/pages/details-hero/details-hero.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Footer, HeaderDetails } from '@/components/organism';
+import { DetailsSection } from '@/components/organism/details-section/details-section';
+import * as S from './details-hero.styles';
+import { useRequestComics } from '@/hooks/use-request-comics/use-request-comics';
+import { useRequestHero } from '@/hooks/use-request-hero/use-request-hero';
+
+const DetailsHeroPage: React.FC = () => {
+  const { hero, loading } = useRequestHero();
+  const { comics, loadingData } = useRequestComics();
+
+  return (
+    <S.Container>
+      <HeaderDetails />
+      <DetailsSection
+        hero={hero}
+        loading={loading}
+        comics={comics}
+        loadingData={loadingData}
+      />
+      <Footer />
+    </S.Container>
+  );
+};
+
+export default DetailsHeroPage;
