eb08793bf8c848583913ac0b94182b491314a8e8
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:28:35 -0300

    test: create test for atom image

---
+++ tests/components/atoms/image/image.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Image } from '@/components/atoms/image/image';
+import { ImageProps } from '@/components/atoms/image/types';
+
+describe('Image Component', () => {
+  const defaultProps: ImageProps = {
+    src: 'test.jpg',
+    descriptionAlt: 'Test image',
+  };
+
+  it('renders the image with correct alt and src attributes', () => {
+    render(<Image {...defaultProps} />);
+    const imgElement = screen.getByAltText(defaultProps.descriptionAlt || '');
+    expect(imgElement).toHaveAttribute('src', defaultProps.src);
+  });
+
+  it('calls the handleClick prop when the image is clicked', () => {
+    const mockHandleClick = jest.fn();
+    render(<Image {...defaultProps} handleClick={mockHandleClick} />);
+    const imgElement = screen.getByAltText(defaultProps.descriptionAlt || '');
+    fireEvent.click(imgElement);
+    expect(mockHandleClick).toHaveBeenCalledTimes(1);
+  });
+});
