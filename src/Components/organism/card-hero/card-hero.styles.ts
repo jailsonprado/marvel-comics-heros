7afcb6a9524df3879e284af6a771b7ba3b1734e1
Jailson Prado <jailsondpd@gmail.com>
2023-08-07 22:39:18 -0300

    feat: create component cardHero

---
+++ src/Components/organism/card-hero/card-hero.styles.ts
@@ -0,0 +1,19 @@
+import { styled } from 'styled-components';
+
+export const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+  }
+`;
---
+++ src/Components/organism/card-hero/card-hero.tsx
@@ -0,0 +1,57 @@
+import {
+  addToFavoriteHero,
+  removeFromFavoriteHero,
+} from '@/redux/heros/heros-slice';
+import { useAppDispatch, useAppSelector } from '@/redux/types/types';
+import { CharacterPaylod } from '@/types/types-response';
+import { CardHero } from '@/components/molecules';
+import * as S from './card-hero.styles';
+import { useNavigate } from 'react-router-dom';
+
+type Props = {
+  data: CharacterPaylod[];
+};
+
+export const CardHeroComponent: React.FC<Props> = ({ data }) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { favoriteHero } = useAppSelector(
+    (rootReducer) => rootReducer.heroSlice,
+  );
+
+  const handleProductClick = (hero: CharacterPaylod) => {
+    dispatch(addToFavoriteHero(hero));
+  };
+
+  const handleRemoveFavoriteHero = (hero: CharacterPaylod) => {
+    dispatch(removeFromFavoriteHero(hero.id));
+  };
+  const handleCardClick = (id: number) => {
+    navigate(`/comics/${id}`);
+  };
+  return (
+    <S.GridContainer>
+      {data.map((item: CharacterPaylod) => {
+        const isFavorited = favoriteHero.some(
+          (favorite) => favorite.id === item.id,
+        );
+        return (
+          <div key={item.id}>
+            <CardHero
+              key={item.id}
+              source={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+              altImage={item?.description}
+              legend={item?.name}
+              handleClick={() => handleProductClick(item)}
+              handleRemove={() => handleRemoveFavoriteHero(item)}
+              isFavorited={isFavorited}
+              data-testid="card-hero"
+              id={item.id}
+              navigateTo={() => handleCardClick(item.id)}
+            />
+          </div>
+        );
+      })}
+    </S.GridContainer>
+  );
+};
