69f0d04a3e2472ce1665570e8396254f276f260c
Jailson Prado <jailsondpd@gmail.com>
2023-08-08 09:33:40 -0300

    feat: create component description hero

---
+++ src/Components/molecules/description-hero/description-hero.styles.ts
@@ -0,0 +1,44 @@
+import { Text } from '@/components/atoms';
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 20px;
+  width: 60%;
+
+  @media (max-width: 768px) {
+    text-align: center;
+    width: 100%;
+  }
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const DescriptionWrapper = styled.div`
+  display: flex;
+
+  width: 100%;
+  flex-direction: column;
+  line-height: 1.8;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const DescriptionText = styled(Text)`
+  width: 60%;
+  @media (max-width: 900px) {
+    width: 100%;
+  }
+`;
---
+++ src/Components/molecules/description-hero/description-hero.tsx
@@ -0,0 +1,55 @@
+import { Favorito01Svg, Favorito02Svg } from '@/assets/svg/svg-mapped';
+import { Image, Text } from '@/components/atoms';
+import * as S from './description-hero.styles';
+
+interface Props {
+  hero?: string;
+  description?: string;
+  isFavorited?: boolean;
+  handleClick: () => void;
+  handleRemove: () => void;
+}
+
+export const DescriptionHero: React.FC<Props> = ({
+  description,
+  hero,
+  handleClick,
+  handleRemove,
+  isFavorited,
+}) => {
+  return (
+    <S.Wrapper>
+      <S.TitleWrapper>
+        <Text component="h1" text={hero} fontSize={40} />
+        {isFavorited && (
+          <Image
+            descriptionAlt="favorite logo remove"
+            src={Favorito01Svg}
+            handleClick={handleRemove}
+            dataTestid="add-favorite-button"
+            width={30}
+            marginLeft={'auto'}
+          />
+        )}
+        {!isFavorited && (
+          <Image
+            descriptionAlt="favorite logo add"
+            src={Favorito02Svg}
+            handleClick={handleClick}
+            dataTestid="add-favorite-button"
+            width={30}
+            marginLeft={'auto'}
+          />
+        )}
+      </S.TitleWrapper>
+      <S.DescriptionWrapper>
+        <S.DescriptionText
+          color="#8C8C8C"
+          fontSize={18}
+          component="p"
+          text={description}
+        />
+      </S.DescriptionWrapper>
+    </S.Wrapper>
+  );
+};
